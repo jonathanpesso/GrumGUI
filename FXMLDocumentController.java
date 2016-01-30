ec0188336e32af31ac6224be309e2ac0145aecf7
jsgau <unknwon2006>
2016-01-29 18:40:38 -0800

    finished uploadpanel

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -13,19 +13,28 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
+/**
+ *
+ * @author jonathan
+ */
 public class FXMLDocumentController implements Initializable {
     
+    @FXML private ListView<String> uploadlist;
     @FXML private TabPane tab_pane;
     @FXML private Tab welcomepanel;
     @FXML private Tab uploadpanel;
@@ -34,47 +43,36 @@ public class FXMLDocumentController implements Initializable {
     @FXML private TextField filepath;
     @FXML private Button btnbrowse;
     @FXML FileChooser filechooser = new FileChooser();
-    @FXML private Desktop desktop = Desktop.getDesktop();
-    
-    
+    @FXML Desktop desktop = Desktop.getDesktop();
+    public File file;
+    final ObservableList<String> listitems = FXCollections.observableArrayList("");
     
     @FXML
-    private void handleChangeUpload(ActionEvent event){
-        tab_pane.getSelectionModel().select(uploadpanel);
-    
-    
-    }
-    @FXML
-    private void handleChangeSearch(ActionEvent event){
-        tab_pane.getSelectionModel().select(searchpanel);
-    
-    
-    }
-    @FXML
-    private void handleUploadButton(ActionEvent event){
-    
-    
-    
-    }
-       @FXML
-    private void handleSearchButton(ActionEvent event){
-    
-    
-    
-    }
-       @FXML
-    private void handleDownloadButton(ActionEvent event){
-    
-    
-    
+    private void handleChangeUpload(){
+        tab_pane.getSelectionModel().select(uploadpanel);    
     }
     
     @FXML
-    private void handleBrowseBtn(ActionEvent event){
-        File file = filechooser.showOpenDialog(filepath.getScene().getWindow());
-        if(file != null)
-            openFile(file);
+    private void handleChangeSearch(){
+        tab_pane.getSelectionModel().select(searchpanel); 
+    }
+    @FXML
+    private void handleBrowseBtn(){
+        /*if(AESCTR.secretKey = null) {
+            notice("Please generate or choose a key!");
+            return;
+        }*/       
         
+        filechooser.setTitle("Select a file to upload");
+        file = filechooser.showOpenDialog(filepath.getScene().getWindow());
+        
+        if(file != null){
+            //ClientWindow.selectedFile = file;
+            //openFile(file);
+            filepath.setText(file.getAbsolutePath());
+            //listitems.add(filepath.getText());
+            writeLog("Selected File: " + filepath.getText());
+        }
     }
     
     private void openFile(File file) {
@@ -87,6 +85,60 @@ public class FXMLDocumentController implements Initializable {
             );
         }
         
+    }
+    @FXML
+    private void handleUploadbtn(){
+        if (filepath.getText() != ""){
+            File fileFromType = new File(filepath.getText());
+                if(fileFromType.isAbsolute() && fileFromType.exists()){
+                    //ClientWindow.selectedFile = fileFromType;
+                } else {
+                    notice("Invalid path to file");
+                    return;
+                }
+        } else {
+            notice("Please select a file");
+            return;
+        }
+						
+        writeLog("Encrypting file...");
+        //for now uses same key to encrypt keywords
+        //String key = UUID.randomUUID().toString();
+        //Map<String, StringPair> map = SSE.EDBSetup(ClientWindow.selectedFile, AESCTR.secretKey, key);
+        //ObjectMapper mapper = new ObjectMapper();
+        /*try {
+            String json = mapper.writeValueAsString(map);
+            System.out.println(json);
+            writeLog("Indexing file...");
+            HttpUtil.HttpPost(json);
+        } catch (JsonProcessingException e1) {
+            e1.printStackTrace();
+            writeLog("Upload failed!");
+            return;
+        }*/
+        writeLog("Uploading file...");
+        //FileUtils.uploadFile(ClientWindow.selectedFile, key, AESCTR.secretKey);
+        //ClientWindow.writeLog("Upload successful!");
+        writeLog("Upload Successful!");
+			
+        
+        
+        
+    }
+    
+    private void writeLog(String info){
+        //uploadlist.append("info");
+        listitems.add(info);
+        uploadlist.setItems(listitems);
+        
+        
+    }
+    private void notice(String note){
+        Alert warn = new Alert(AlertType.INFORMATION);
+        warn.setTitle("Notice!");
+        warn.setHeaderText(null);
+        warn.setContentText(note);
+               
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
